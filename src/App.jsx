7fe1244e2a7d22a14d6c5caa9c820f287e85cc2e
rabby-shek <rabbyshek1998@gmail.com>
2025-08-07 17:50:02 +0600

    hero section created for large screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
 import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <>
-    <NavBar />
-      app
-    </>
+    <main>
+      <NavBar />
+      <HeroSection />
+      <div className='h-dvh'></div>
+    </main>
   )
 }
 
-export default App
+export default App;
