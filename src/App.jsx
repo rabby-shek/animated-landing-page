545786f336bd23418596d0660ab2dd4c993ee97a
rabby-shek <rabbyshek1998@gmail.com>
2025-08-07 16:15:03 +0600

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <NavBar />
+      app
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='fixed top-0 left-0 md:p-8 p-3'>
+      <img src="/images/nav-logo.svg" alt="" />
+    </div>
+  )
+}
+
+export default NavBar
